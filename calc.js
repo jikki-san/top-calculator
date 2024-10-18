26b49cbc7bf12fffcbb05b474cb0b87e7f36ebbd
Jackson Eckert <jaxon.eckert@gmail.com>
2024-10-17 21:17:14 -0500

    Add event handlers for updating the display

--- calc.js
+++ calc.js
@@ -1,6 +1,7 @@
 let a = 0;
 let b = 0;
 let op = "+";
+let displayValue = "";
 
 function add(a, b) {
   return a + b;
@@ -31,3 +32,31 @@ function operate(a, b, op) {
     // TODO: add a default case
   }
 }
+
+function dispatchDisplayUpdate() {
+  const displayUpdateEvent = new Event("displayUpdate");
+  display.dispatchEvent(displayUpdateEvent);
+}
+
+function handleDigitClicked(event) {
+  const button = event.target;
+  displayValue += button.textContent;
+  dispatchDisplayUpdate();
+}
+
+function handleClearClicked() {
+  displayValue = "";
+  dispatchDisplayUpdate();
+}
+
+function updateDisplay() {
+  display.textContent = displayValue;
+}
+
+const digitsContainer = document.querySelector(".digits");
+const clearButton = document.querySelector("#clear");
+const display = document.querySelector(".display");
+
+digitsContainer.addEventListener("click", handleDigitClicked);
+clearButton.addEventListener("click", handleClearClicked);
+display.addEventListener("displayUpdate", updateDisplay);
