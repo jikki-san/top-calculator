7ed52d50212f30bac99b51fd63d9f5b11262161e
Jackson Eckert <jaxon.eckert@gmail.com>
2024-10-17 20:02:57 -0500

    Add basic operation functions

--- calc.js
+++ calc.js
@@ -1,1 +1,15 @@
-console.log("Beep boop...");
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
